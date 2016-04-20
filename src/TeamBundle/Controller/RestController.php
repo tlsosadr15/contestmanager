c41f7b1bf15e48fdb147167316bbd49aefc71729
Guillaume <guillaume.torres91@gmail.com>
2016-04-20 16:40:48 +0200

    Mise en place doc api

--- src/TeamBundle/Controller/RestController.php
+++ src/TeamBundle/Controller/RestController.php
@@ -9,10 +9,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\Serializer\SerializationContext;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class RestController extends Controller
 {
     
+    /**
+     * @ApiDoc(
+     * resource=true,
+     * resourceDescription="Operations on teams",
+     * description= "Get all teams",
+     * output= "UserBundle\Entity\User"
+     * )
+    */
     public function getTeamsAction()
     {
         $em = $this->getDoctrine()->getManager();
@@ -22,6 +31,22 @@ class RestController extends Controller
         return $teams;
     }
     
+    /**
+     * @ApiDoc(
+     * resource=true,
+     * resourceDescription="Operations on teams",
+     * description= "Get team by id",
+     * output="TeamBundle\Entity\Team",
+     * requirements={
+     *      {
+     *          "name"="id",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="Id of the team"
+     *      }
+     *  }
+     * )
+    */
     public function getTeamAction($id)
     {
         $em = $this->getDoctrine()->getManager();
--- src/UserBundle/Controller/RestController.php
+++ src/UserBundle/Controller/RestController.php
@@ -10,10 +10,18 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\Serializer\SerializationContext;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class RestController extends Controller
 {
-    
+    /**
+     * @ApiDoc(
+     * resource=true,
+     * resourceDescription="Operations on users",
+     * description= "Get all users",
+     * output= "UserBundle\Entity\User"
+     * )
+    */
     public function getUsersAction()
     {
         $em = $this->getDoctrine()->getManager();
@@ -23,13 +31,54 @@ class RestController extends Controller
         return $users;
     }
     
-    public function postUsersLoginAction(Request $request){
+    /**
+     * @Rest\Post("/user/login", name="_login")
+     * @ApiDoc(
+     * resource=true,
+     * resourceDescription="Operations on users",
+     * description= "User login",
+     * parameters={
+     *      {"name"="username", "dataType"="string", "required"=true, },
+     *      {"name"="password", "dataType"="string", "required"=true, }
+     *  }
+     * )
+    */
+    public function loginAction(Request $request){
         $username = $request->get('username');
         $password = $request->get('password');
-        exit($username);
+        exit($password);
         return false;
+        
+        $user = $this->get('fos_user.user_manager')->findUserBy(array('username' => $email));
+        $logger = $this->get('logger');
+        if( empty($user) ){
+            return false;
+        }
+        $logger->info('user found for '.$email);
+        $encoder = $this->get('security.encoder_factory')->getEncoder($user);
+        if (!$encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt())) {
+            return false;
+        }
+        
+        return $user;
     }
     
+    /**
+     * @ApiDoc(
+     * resource=true,
+     * section="Operations on users",
+     * description= "Get user by id",
+     * output="UserBundle\Entity\User",
+     * requirements={
+     *      {
+     *          "name"="id",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="Id of the user"
+     *      }
+     *  }
+     * )
+    */
     public function getUserAction($id)
     {
         $em = $this->getDoctrine()->getManager();
@@ -37,5 +86,25 @@ class RestController extends Controller
         $user = $em->getRepository('UserBundle:User')->findOneBy(array('id' => $id));
         
         return $user;
+    }
+    
+    /**
+     * @ApiDoc(
+     * resource=true,
+     * resourceDescription="Operations on users",
+     * description= "Edit user by id",
+     * requirements={
+     *      {
+     *          "name"="id",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="Id of the user"
+     *      }
+     *  }
+     * )
+    */
+    public function putUserAction($id)
+    {
+        exit('edit');
     }
 }

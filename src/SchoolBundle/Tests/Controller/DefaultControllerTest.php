b3052d8321e2c99783c0f01a0f7af5109e4661bb
Guillaume <guillaume.torres91@gmail.com>
2016-12-08 17:43:18 +0100

    Fix test fonctionnel

--- src/SchoolBundle/Tests/Controller/DefaultControllerTest.php
+++ src/SchoolBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,36 +15,7 @@ class DefaultControllerTest extends WebTestCase
     public function testList()
     {
         $client = static::createClient();
-        $client->request('GET', '/school/school/list');
+        $client->request('GET', '/school/list');
         $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
-
-    /**
-     * Test create news
-     *
-     * @return null
-     */
-//    public function testCreate()
-//    {
-//        $client = static::createClient();
-//        $crawler = $client->request('GET', '/school/school/create');
-//        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Create', $crawler->filter('title')->text());
-//
-//        $action = $crawler->filter('div.sonata-ba-form form')->attr('action');
-//        $formId = explode('=', $action)[1];
-//
-//        $form = $crawler->selectButton('btn_create_and_list')->form();
-//
-//        $form->setValues(array(
-//            $formId => array(
-//                'name' => 'A school',
-//            ),
-//        ));
-//
-//        $client->submit($form);
-//        $crawler = $client->followRedirect();
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Item "A school" has been successfully created.', $crawler->filter('div.alert-success')->text());
-//    }
 }

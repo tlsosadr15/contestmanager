92df8c2a4724817a22ed56920456eb17f3a76293
Guillaume <guillaume.torres91@gmail.com>
2016-12-09 15:12:49 +0100

    BO: Administration utilisateurs

---
+++ src/CoreBundle/Admin/UserAdmin.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * UserAdmin class file
+ *
+ * PHP Version 7.0
+ *
+ * @category Admin
+ * @package  CoreBundle\Admin
+ * @author   Guillaume <guillaume.torres91@gmail.com>
+ * @license  All right reserved
+ * @link     Null
+ */
+namespace CoreBundle\Admin;
+
+use MatchBundle\Entity\GroupMatch;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use UserBundle\Entity\User;
+
+/**
+ * Class UserAdmin
+ *
+ * @category Admin
+ * @package  CoreBundle\Admin
+ * @author   Guillaume <guillaume.torres91@gmail.com>
+ * @license  All right reserved
+ * @link     Null
+ */
+class UserAdmin extends AbstractAdmin
+{
+    protected $baseRoutePattern = 'user';
+
+    /**
+     * @param User $object Team object
+     *
+     * @return string
+     */
+    public function toString($object)
+    {
+        return $object instanceof GroupMatch
+            ? 'User '.$object->getUsername()
+            : 'User';
+    }
+
+    /**
+     * @param User $object User
+     */
+    public function prePersist($object) {
+        $object->setEnabled(true);
+        $object->setRoles(array('ROLE_ADMIN'));
+    }
+
+    /**
+     * @param FormMapper $formMapper Form mapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('username', TextType::class)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('plainPassword', PasswordType::class)
+            ->add('school', 'entity', array(
+                    'class' => 'SchoolBundle\Entity\School',
+                    'multiple' => false,
+                    'required' => false,
+                )
+            );
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper Datagrid mapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('username');
+    }
+
+    /**
+     * @param ListMapper $listMapper List mapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('username')
+            ->add('firstName')
+            ->add('lastName');
+    }
+}

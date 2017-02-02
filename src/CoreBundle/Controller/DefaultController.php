58937504aa23fc969bba07a23e0df9c3c3892a7c
Guillaume <guillaume.torres91@gmail.com>
2017-02-02 10:37:29 +0100

    Projet BI

--- src/CoreBundle/Controller/DefaultController.php
+++ src/CoreBundle/Controller/DefaultController.php
@@ -14,6 +14,8 @@ namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * DefaultController class
@@ -34,6 +36,32 @@ class DefaultController extends Controller
     public function tournamentAction()
     {
         return new RedirectResponse('/');
+    }
+
+    /**
+     * tournament action
+     *
+     * @return RedirectResponse
+     */
+    public function selectAction(Request $request)
+    {
+        if ($request->isMethod('GET')) {
+            $data = file_get_contents('https://data.iledefrance.fr/api/records/1.0/search/?dataset=liste_des_etablissements_publics_culture&rows=20&facet=ville&ville=PARIS');
+            $records = json_decode($data)->records;
+            $items = [];
+            foreach ($records as $record) {
+                $items[$record->fields->adresse] = $record->fields->structure;
+            }
+
+            return new Response($this->render('UserBundle:Default:index.html.twig', array('items' => $items)));
+        }
+        elseif ($request->isMethod('POST')) {
+            $data = $request->request->all();
+            $url = 'https://www.google.fr/maps/place/'.$data['address'];
+
+            return new RedirectResponse($url);
+        }
+
     }
 
 }

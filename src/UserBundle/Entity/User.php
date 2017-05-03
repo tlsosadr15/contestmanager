e84803b7a45b1cc8f5a4a1255b9b7e599d7791c1
guillaume <guillaume.torres91@gmail.com>
2017-05-03 16:36:50 +0200

    Test avec php7

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -58,6 +58,7 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
+        $this->salt = null;
         $this->group = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
